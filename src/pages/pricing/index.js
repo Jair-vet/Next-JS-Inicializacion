d756d7a6c2990d8ad2e403d1db0c38d30cd28c50
Jair <jairaceves56@gmail.com>
2023-04-21 10:35:58 -0600

    Creamos una nueva pagina Pricing

---
+++ src/pages/pricing/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { Navbar } from '@/components/Navbar'
+import { MainLayout } from '@/components/layout/MainLayout'
+
+export default function PricingPage() {
+  return (
+    <MainLayout>
+      <h1 className='text-3xl text-center mb-6 font-bold'>Pricing Page</h1>
+      <h2 className='text-center text-5xl font-bold'>
+        Ir a <Link href="/" className='text-darkBlue font-bold'>HomePage</Link>
+      </h2>
+
+      <p className='text-2xl text-center mt-20'>
+        Get started by editing&nbsp;
+        <code className='text-sm bg-gray-50 p-2 rounded-md'>src/pricing/index.js</code>
+      </p>
+    </MainLayout>
+  )
+}
