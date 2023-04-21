079362e249c12b9130471da77253bf86f9a54a81
Jair <jairaceves56@gmail.com>
2023-04-20 18:56:32 -0600

    App basica para Aprender

---
+++ src/pages/about.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+import { Navbar } from '@/components/Navbar'
+
+export default function AboutPagev() {
+  return (
+    <>
+      <Head>
+        <title>About - Jair</title>
+        <meta name="description" content="Home Page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>
+
+        <h1 className={styles.title}>
+        Regresar a <Link href="/">HomePage</Link>
+        </h1>
+
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/pages/index.js</code>
+        </p>
+
+      </main>
+    </>
+  )
+}
