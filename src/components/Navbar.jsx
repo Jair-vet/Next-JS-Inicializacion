8fa6223252f7388b322f1d01d8fc842c82593650
Jair <jairaceves56@gmail.com>
2023-04-21 10:36:28 -0600

    Hicimos un Navbar con un Arreglo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,41 @@
 import { ActiveLink } from './ActiveLink'
 
+const menuItems = [
+  {
+      text: 'Home',
+      href: '/'
+  },
+  {
+      text: 'About',
+      href: '/about'
+  },
+  {
+      text: 'Contact',
+      href: '/contact'
+  },
+  {
+      text: 'Pricing',
+      href: '/pricing'
+  },
+];
 
 export const Navbar = () => {
   return (
     <>
         <nav className='text-center text-2xl m-2 p-2 font-bold'>
-            <ActiveLink text="Home" href="/"/>
+          {
+            menuItems.map( ({ text, href }) => (
+              <ActiveLink 
+                key={href}
+                text={text}
+                href={href}
+              />
+            ))
+          }
+            {/* <ActiveLink text="Home" href="/"/>
             <ActiveLink text="About" href="/about"/>
             <ActiveLink text="Contact" href="/contact"/>
+            <ActiveLink text="Pricing" href="/pricing"/> */}
         </nav>
     </>
   )
