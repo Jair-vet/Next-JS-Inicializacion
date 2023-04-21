e52db3abd453e2fcd3509e6c82fe6aba4dddce21
Jair <jairaceves56@gmail.com>
2023-04-20 20:11:22 -0600

    Creamos un Navbar Dinamico

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import Link from 'next/link'
+import styles from './Navbar.module.css'
+import { ActiveLink } from './ActiveLink'
 
 
 export const Navbar = () => {
   return (
     <>
-        <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
+        <nav className={ styles['menu-container'] }>
+            <ActiveLink text="Home" href="/"/>
+            <ActiveLink text="About" href="/about"/>
+            <ActiveLink text="Contact" href="/contact"/>
         </nav>
     </>
   )
